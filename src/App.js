528c84483e2d56b16efa359a6b5711a4b9e4f3c7
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2023-11-14 14:28:25 +0200

    added react and Boosrtap to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Weather App</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        <p>
+          Coded by Nthabi at{" "}
+          <a href="https://www.shecodes.io/" target="_blank">
+            SheCodes
+          </a>{" "}
+          and open sourced on <a>GidHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
