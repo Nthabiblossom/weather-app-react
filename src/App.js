3530ce62613f7a91cd96ea58e252f126f2df7fdf
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2023-11-22 13:44:04 +0200

    added css to the app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        Coded by Nthabi at{" "}
-        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-          SheCodes
-        </a>{" "}
-        and open sourced on{" "}
-        <a
-          href="https://github.com/Nthabiblossom"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <br />
+        <footer>
+          Coded by Nthabi at{" "}
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            SheCodes
+          </a>{" "}
+          and open sourced on{" "}
+          <a
+            href="https://github.com/Nthabiblossom"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,37 +3,40 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather mt-5">
-      <div className="row">
-        <form>
-          <input
-            type="search"
-            placeholder="enter a city"
-            className="formControl col-9"
-          />
-          <button className="btn btn-success col-3">search</button>
-        </form>
-        <h1>Pretoria</h1>
-      </div>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-        alt="partly cloudy"
-      />
-
-      <div>
-        <h3>32℃</h3>
-      </div>
-      <div>
-        <h3>Clear sky</h3>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <p>25 %</p>
-          <p>Humidity</p>
+    <div className="Weather mt-5 ">
+      <div className="contents position-relative">
+        <div className="row ">
+          <form className="mt-3">
+            <input
+              type="search"
+              placeholder="enter a city"
+              className="formControl col-9"
+            />
+            <button className="btn btn-success col-3">search</button>
+          </form>
+          <h1 className="">Pretoria</h1>
         </div>
-        <div className="col-6">
-          <p>27 km/h</p>
-          <p>Wind Speed</p>
+        <div className="weatherIcon">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly cloudy"
+          />
+        </div>
+        <div>
+          <h3>32℃</h3>
+        </div>
+        <div>
+          <h3 className="mb-3">Clear sky</h3>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <p>25 %</p>
+            <p>Humidity</p>
+          </div>
+          <div className="col-6">
+            <p>27 km/h</p>
+            <p>Wind Speed</p>
+          </div>
         </div>
       </div>
     </div>
